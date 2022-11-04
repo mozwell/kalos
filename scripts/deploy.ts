da70f45751793adb459a637ed766ebdd1c9440f7
mozwell <mozwell.2020@gmail.com>
2022-11-04 15:05:39 +0800

    add user-friendly log in deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,6 +8,31 @@ async function main() {
   const MAX_DEPLOYER_ARTWORKS = 10;
 
   const Contract = await ethers.getContractFactory("Kalos");
+
+  const gasPrice = await Contract.signer.getGasPrice();
+  console.log(`Current gas price: ${gasPrice}`);
+  const estimatedGas = await Contract.signer.estimateGas(
+    Contract.getDeployTransaction(
+      MAX_TOTAL_ARTWORKS,
+      MAX_PERSONAL_ARTWORKS,
+      MAX_DEPLOYER_ARTWORKS
+    )
+  );
+  console.log(`Estimated gas: ${estimatedGas}`);
+
+  const deploymentPrice = gasPrice.mul(estimatedGas);
+  const deployerBalance = await Contract.signer.getBalance();
+  console.log(
+    `Deployer balance:  ${ethers.utils.formatEther(deployerBalance)}`
+  );
+  console.log(
+    `Deployment price:  ${ethers.utils.formatEther(deploymentPrice)}`
+  );
+
+  if (Number(deployerBalance) < Number(deploymentPrice)) {
+    throw new Error("You dont have enough balance to deploy.");
+  }
+
   const contractInstance = await Contract.deploy(
     MAX_TOTAL_ARTWORKS,
     MAX_PERSONAL_ARTWORKS,
@@ -31,6 +56,7 @@ function writeContractInfo(contractInstance: any) {
     path.resolve("./frontend/src/config/contractInfo.json"),
     JSON.stringify(data)
   );
+  console.log("Kalos contract info is written successfully");
 }
 
 main().catch((error) => {
